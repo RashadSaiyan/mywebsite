c8a21abccf582e6fe939a7e00908f387d6125730
Rashad Nelson <rashadn465@gmail.com>
2022-09-13 21:43:48 -0400

    Footer and JS

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log("Hello World");
+
+const age = 23;
+
+const canYouDrink = (age) => {
+if (age > 21){
+    console.log("Go ahead and drink")
+}else{
+    console.log("Not yet here is some milk")
+}
+};
+
+canYouDrink(age);
